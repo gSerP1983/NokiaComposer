029d29f5d2a5ab09d4b4749ad1329ec3a0ae04cd
serp <gserp1983@gmail.com>
2017-04-18 23:38:36 +0300

    1) gradle: firebase-crash update, firebase-database add 2) largeHeap="true" 3) FirebaseDatabase begin

--- app/src/main/java/com/serp1983/nokiacomposer/App.java
+++ app/src/main/java/com/serp1983/nokiacomposer/App.java
@@ -1,13 +1,37 @@
 package com.serp1983.nokiacomposer;
 
 import android.app.Application;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.serp1983.nokiacomposer.domain.RingtoneDTO;
 import com.serp1983.nokiacomposer.logic.DataService;
-import com.serp1983.nokiacomposer.util.InterstitialAdService;
-import com.serp1983.nokiacomposer.util.RewardedVideoAdService;
+
 
 public class App extends Application {
     public void onCreate() {
         super.onCreate();
         DataService.initialize(this);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.setPersistenceEnabled(true);
+        DatabaseReference databaseRef = database.getReference();
+        databaseRef.keepSynced(true);
+
+        databaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    RingtoneDTO ringtone = postSnapshot.getValue(RingtoneDTO.class);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                databaseError.toException().printStackTrace();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/serp1983/nokiacomposer/domain/RingtoneDTO.java
@@ -0,0 +1,12 @@
+package com.serp1983.nokiacomposer.domain;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class RingtoneDTO {
+    public String Name;
+    public String Code;
+    public int Tempo;
+
+    public RingtoneDTO(){}
+}
