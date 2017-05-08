3e5714d0073de80862fa6112f5733c03032e4d9a
serp <gserp1983@gmail.com>
2017-05-09 00:41:42 +0300

    prepare use firebase

--- app/src/main/java/com/serp1983/nokiacomposer/logic/FirebaseDatabaseService.java
+++ app/src/main/java/com/serp1983/nokiacomposer/logic/FirebaseDatabaseService.java
@@ -3,6 +3,7 @@ package com.serp1983.nokiacomposer.logic;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.serp1983.nokiacomposer.domain.RingtoneDTO;
+import com.serp1983.nokiacomposer.domain.RingtoneVM;
 
 public class FirebaseDatabaseService {
     public static void initialize(){
@@ -17,8 +18,17 @@ public class FirebaseDatabaseService {
     }
 
     public static void add(RingtoneDTO ringtone){
-        if (ringtone == null || ringtone.Code.length() < 50)
+        if (ringtone == null || ringtone.Code.length() < 40)
             return;
+        if (ringtone.Name.startsWith("(My)"))
+            return;
+        String code = ringtone.Code.substring(10, 40).replaceAll("\\s", "").toUpperCase();
+        for(RingtoneVM assetRingtone : DataService.getInstance().getAssetRingtones()) {
+            String assetCode = assetRingtone.getCode().replaceAll("\\s", "").toUpperCase();
+            if (assetCode.contains(code))
+                return;
+        }
+
         FirebaseDatabaseService.getReference().push().setValue(ringtone);
     }
 
